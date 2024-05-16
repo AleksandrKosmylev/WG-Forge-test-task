b9c69ee1180ed69ce873767395ffba5ae11e8409
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-16 15:22:41 +0300

    add app

---
+++ service/server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to Flask!'
+
+
+@app.route("/ping")
+def ping():
+    return "Cats Service. Version 0.1"
+
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080)
